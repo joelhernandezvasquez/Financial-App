085567a37998294e2fa7b333541b9eeb731ab09b
Joel Hernandez <jhernandez98@outlook.es>
2025-01-13 06:37:40 -0500

    add minimize button to the navigation

--- src/ui/sidenav/SideNav.tsx
+++ src/ui/sidenav/SideNav.tsx
@@ -1,11 +1,15 @@
 
+import { FinanceLogo } from '../finance-logo/FinanceLogo';
 import NavLinks from '../nav-links/NavLinks';
+import MinimizeMenuBtn from './MinimizeMenuBtn';
 import style from './style.module.css';
 
 export const SideNav = () => {
   return (
     <nav className={style.sidenav}>
+      <FinanceLogo/>
       <NavLinks/>
-    </nav>
+      <MinimizeMenuBtn/>
+     </nav>
   )
 }
