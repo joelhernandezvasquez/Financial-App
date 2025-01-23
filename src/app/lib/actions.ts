b721d439e56906bc9b8e33bc6d65216a595f8863
Joel Hernandez <jhernandez98@outlook.es>
2025-01-23 09:41:07 -0500

    fix endpoint url

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,11 +1,11 @@
 import { Balance } from "./definitions";
 
-const {HOST} = process.env;
+//const {HOST} = process.env;
 
 export const fetchBalanceSummary = async ():Promise<Balance> =>{
   try{
     
-    const request = await fetch(`${HOST}/balance`);
+    const request = await fetch(`http://localhost:4000/api/balance`);
 
     if(!request.ok) throw Error('Something went wrong while fetching balance');
     const response = await request.json();
