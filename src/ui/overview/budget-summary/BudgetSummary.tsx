9da95de64450da6ac12e3c791e2994977a258c77
Joel Hernandez <jhernandez98@outlook.es>
2025-02-04 06:32:17 -0500

    add action to fetch budgets

--- src/ui/overview/budget-summary/BudgetSummary.tsx
+++ src/ui/overview/budget-summary/BudgetSummary.tsx
@@ -1,38 +1,13 @@
 import SubTitle from '@/ui/sub-title/SubTitle';
 import LinkButton from '@/ui/link-button/LinkButton';
 import Image from 'next/image';
-import { Budget } from '@/lib/definitions';
 import PotsCategory from '../pots-summary/pots-category/PotsCategory';
+import { getBudgets } from '@/lib/actions';
 import style from './style.module.css';
 
-const budgets:Budget[] =  [
-        {
-            id:1,
-            category: "Entertainment",
-            maximum: 50.00,
-            theme: "#277C78"
-        },
-        {
-            id:2,
-            category: "Bills",
-            maximum: 750.00,
-            theme: "#82C9D7"
-        },
-        {
-            id:3,
-            category: "Dining Out",
-            maximum: 75.00,
-            theme: "#F2CDAC"
-        },
-        {
-            id:4,
-            category: "Personal Care",
-            maximum: 100.00,
-            theme: "#626070"
-        }
-    ]
+export const BudgetSummary = async () => {
+  const budgets = await getBudgets();
 
-export const BudgetSummary = () => {
   return (
     <section className={style.budget_container}>
       <header className={style.budget_header}>
