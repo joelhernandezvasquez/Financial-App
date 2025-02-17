065c97eb225a766900ba8e06295534254c22e22b
Joel Hernandez <jhernandez98@outlook.es>
2025-02-17 09:09:22 -0500

    create action to get summary bills

--- src/ui/overview/recurring-bills-summary/RecurringBillsSummary.tsx
+++ src/ui/overview/recurring-bills-summary/RecurringBillsSummary.tsx
@@ -1,31 +1,12 @@
 import LinkButton from '@/ui/link-button/LinkButton';
 import SubTitle from '@/ui/sub-title/SubTitle';
-import style from './style.module.css';
-import { Bill } from '@/lib/definitions';
 import RecurringBillItem from './recurring-bill-item/RecurringBillItem';
+import { getTransactionSummary } from '@/lib/actions';
+import style from './style.module.css';
 
-const paidBills:Bill[] = [
-    {
-        id:1,
-        category:'Paid Bills',
-        amount:190.00,
-        theme:'#277C78'
-    },
-    {
-        id:2,
-        category:'Total Upcoming',
-        amount:194.98,
-        theme:'#F2CDAC' 
-    },
-    {
-        id:3,
-        category:'Due Soon',
-        amount:59.98,
-        theme:'#82C9D7'  
-    }
-]
+const RecurringBillsSummary = async() => {
+  const bills = await getTransactionSummary();
 
-const RecurringBillsSummary = () => {
   return (
     <section className={style.bills_container}>
         <header className={style.bills_header}>
@@ -34,7 +15,7 @@ const RecurringBillsSummary = () => {
      </header>
 
      <ul className={style.bills_items}>
-       {paidBills.map((bill)=>{
+       {bills.map((bill)=>{
          return <RecurringBillItem key={bill.id} bill={bill}/>
        })}
      </ul>
