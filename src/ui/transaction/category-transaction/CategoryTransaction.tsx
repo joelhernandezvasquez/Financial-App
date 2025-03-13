fcfa19f7db91bc4e548c902e8ae5f1e7a1c8b3df
Joel Hernandez <jhernandez98@outlook.es>
2025-03-13 09:19:54 -0400

    filter by category

--- src/ui/transaction/category-transaction/CategoryTransaction.tsx
+++ src/ui/transaction/category-transaction/CategoryTransaction.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { useState } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import UseToggle from '@/hooks/use-toggle/UseToggle';
 import UseClickAway from '@/hooks/click-away/UseClickAway';
 import Image from 'next/image';
 import Dropdown from '@/ui/dropdown/Dropdown';
 import style from './style.module.css';
+
 interface Props{
   categories: string[]
 }
@@ -13,9 +15,23 @@ const CategoryTransaction = ({categories}:Props) => {
   const {isToggle,handleToggle} = UseToggle();
   const [currentCategory,setCategory] = useState('All Transactions');
   const dropdownRef = UseClickAway(handleToggle);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const {replace} = useRouter();
 
   const handleOnClickCategory = (value:string) =>{
     setCategory(value);
+   
+    const params = new URLSearchParams(searchParams);
+    params.set('page','1');
+
+    if(value){
+      params.set('query', value);
+    }
+    else{
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
     handleToggle();
   } 
 
